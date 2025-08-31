dcb8c56392d1fb21f9e5d0691d5d3df65fa4aeb9
sourav-020121996 <das.sour@northeastern.edu>
2025-08-31 14:33:08 -0400

    Tests for normalize method in FaissIndex

---
+++ app/controllers/benchMarkController.py
@@ -0,0 +1,2 @@
+def runBenchmark():
+  pass
--- test.py
+++
@@ -1,5 +0,0 @@
-from app.embedding_model import EmbeddingModel
-
-model = EmbeddingModel("all-MiniLM-L6-v2")
-sentence = ["I love books"]
-print(model.encode(sentence))
